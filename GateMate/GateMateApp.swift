ea6e9dde48720d47a823746f4b2a87678a02fbd8
ivandemicco00 <151406012+ivandemicco00@users.noreply.github.com>
2026-02-16 16:04:38 +0100

    Initial Commit

---
+++ GateMate/GateMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GateMateApp.swift
+//  GateMate
+//
+//  Created by Ivan De Micco on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GateMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
